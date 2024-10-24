8eaaafa895c399ea134654de309b7223fb83a808
ahmed afzal <ahmed.afzal2070@gmail.com>
2024-10-25 02:20:26 +0500

    update product path

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import Banner from "../components/banner/banner";
 import Footer from "../components/footer/Footer";
 import Navigation from "../components/navigation/Navigation";
-import Products from "../components/products/products";
+import Products from "../components/products/Products";
 import SellingFireworks from "../components/sellingFireworks/SellingFireworks";
 
 const Home = () => {
